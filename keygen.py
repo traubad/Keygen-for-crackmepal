870f7147827174ebdb6fe9697e52a62c6407792b
traubad <traubad@vcu.edu>
2020-05-14 17:16:20 -0400

    First commit (it's also done)

---
+++ keygen.py
@@ -0,0 +1,24 @@
+import string
+from random import choice, randint
+from subprocess import PIPE, run
+
+
+def generate_key():
+    possible_chars = string.ascii_letters + string.digits
+    key = [choice(possible_chars) for i in range(8)]
+    key[randint(0, len(key)-1)] = '@'
+    key.insert(4, '-')
+    return ''.join(key)
+
+
+def test_key_gen(program, test_count):
+    for i in range(test_count):
+        key = generate_key()
+        print(f"test {i+1}: {key}")
+        x = run([program, key], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        assert(x.returncode == 0)
+    print(f"completed {test_count} successful runs!")
+    return True
+
+test_key_gen('./crackmeMario', 10)
+
